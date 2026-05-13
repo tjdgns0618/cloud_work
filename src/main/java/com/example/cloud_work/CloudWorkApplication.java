7a5914b1171ba1f4e9195e89dd64a923b87b333b
tjdgns0618 <gruddufs@gmail.com>
2026-05-13 17:26:01 +0900

    Initial commit

---
+++ src/main/java/com/example/cloud_work/CloudWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloud_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudWorkApplication.class, args);
+    }
+
+}
